000b442703f0c36c34cf45c95ddd199beccc0b4e
Maxim Makovskiy <makovskiyms@gmail.com>
2022-11-11 16:19:37 +0600

    Add grid of cards with books

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,6 +1,8 @@
 const library = [];
 
 const form = document.querySelector("form");
+const booksGrid = document.querySelector("#books");
+
 form.addEventListener("submit", addBookToLibrary);
 
 function Book(title, author, pages, read) {
@@ -22,4 +24,28 @@ function addBookToLibrary(e) {
   library.push(book);
 
   form.reset();
+}
+
+function addBookToGrid() {
+  for (const book of library) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book");
+
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    read.textContent = book.read ? "Read it" : "Haven't read it yet";
+
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+
+    booksGrid.appendChild(bookCard);
+  }
 }
