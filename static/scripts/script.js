a2665890e43ebd5b118548cc171041a180296b16
Maxim Makovskiy <makovskiyms@gmail.com>
2022-11-11 22:38:16 +0600

    Add `add` button and modal form

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,14 @@
 const library = [];
 
+const modal = document.querySelector("#modal");
 const form = document.querySelector("form");
 const booksGrid = document.querySelector("#books");
+const addBtn = document.querySelector("#add-btn");
+const closeBtn = document.querySelector("#close-btn");
 
 form.addEventListener("submit", addBookToLibrary);
+addBtn.addEventListener("click", e => modal.style.display = "block");
+closeBtn.addEventListener("click", closeForm);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -23,29 +28,37 @@ function addBookToLibrary(e) {
   const book = new Book(title, author, pages, read);
   library.push(book);
 
-  form.reset();
+  closeForm();
+
+  addBookToGrid();
 }
 
 function addBookToGrid() {
-  for (const book of library) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book");
+  const book = library[library.length - 1];
 
-    const title = document.createElement("p");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const read = document.createElement("p");
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book");
+  bookCard.dataset.index = library.length - 1;
 
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-    read.textContent = book.read ? "Read it" : "Haven't read it yet";
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const read = document.createElement("p");
 
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(read);
+  title.textContent = `Title: ${book.title}`;
+  author.textContent = `Author: ${book.author}`;
+  pages.textContent = `Number of pages: ${book.pages}`;
+  read.textContent = book.read ? "Read it" : "Haven't read it yet";
 
-    booksGrid.appendChild(bookCard);
-  }
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  bookCard.appendChild(read);
+
+  booksGrid.appendChild(bookCard);
+}
+
+function closeForm() {
+  modal.style.display = "none";
+  form.reset();
 }
