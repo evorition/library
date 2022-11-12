aacfd13e3b40eb84035fe76a700b170f9fc00ff5
Maxim Makovskiy <makovskiyms@gmail.com>
2022-11-12 17:25:56 +0600

    Add `delete` button

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,14 +1,14 @@
 const library = [];
 
-const modal = document.querySelector("#modal");
+const modalForm = document.querySelector("#modal-form");
 const form = document.querySelector("form");
 const booksGrid = document.querySelector("#books");
-const addBtn = document.querySelector("#add-btn");
-const closeBtn = document.querySelector("#close-btn");
+const addButton = document.querySelector("#add-btn");
+const closeButton = document.querySelector("#close-btn");
 
 form.addEventListener("submit", addBookToLibrary);
-addBtn.addEventListener("click", e => modal.style.display = "block");
-closeBtn.addEventListener("click", closeForm);
+addButton.addEventListener("click", e => modalForm.style.display = "block");
+closeButton.addEventListener("click", closeForm);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -29,36 +29,60 @@ function addBookToLibrary(e) {
   library.push(book);
 
   closeForm();
-
-  addBookToGrid();
+  addBookToGrid(book);
 }
 
-function addBookToGrid() {
-  const book = library[library.length - 1];
-
+function addBookToGrid(book) {
   const bookCard = document.createElement("div");
   bookCard.classList.add("book");
-  bookCard.dataset.index = library.length - 1;
 
   const title = document.createElement("p");
   const author = document.createElement("p");
   const pages = document.createElement("p");
   const read = document.createElement("p");
+  const deleteButton = document.createElement("button");
+
+  deleteButton.classList.add("delete-btn");
 
   title.textContent = `Title: ${book.title}`;
   author.textContent = `Author: ${book.author}`;
   pages.textContent = `Number of pages: ${book.pages}`;
   read.textContent = book.read ? "Read it" : "Haven't read it yet";
+  deleteButton.textContent = "Delete";
 
   bookCard.appendChild(title);
   bookCard.appendChild(author);
   bookCard.appendChild(pages);
   bookCard.appendChild(read);
+  bookCard.appendChild(deleteButton);
 
   booksGrid.appendChild(bookCard);
+
+  addDeleteButton(deleteButton);
+  updateIndex();
 }
 
 function closeForm() {
-  modal.style.display = "none";
+  modalForm.style.display = "none";
   form.reset();
+}
+
+function addDeleteButton(deleteButton) {
+  deleteButton.addEventListener("click", () => {
+    const bookElement = deleteButton.parentElement;
+    const bookIndex = bookElement.dataset.index;
+
+    bookElement.remove();
+    library.splice(bookIndex, 1);
+
+    updateIndex();
+  });
+}
+
+function updateIndex() {
+  const bookElements = booksGrid.children;
+
+  for (let i = 0; i < bookElements.length; ++i) {
+    bookElements[i].dataset.index = i;
+  }
 }
